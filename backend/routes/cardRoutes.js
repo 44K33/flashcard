0c9627240826ba948fc6ff4c2fadca3f08565cd7
Kostadin Kostadinov <43kostadinov@gmail.com>
2026-06-30 11:21:27 +0200

    MongoDB Lokal erstellt und Server aufgesetzt

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,1 +1,17 @@
 // /api/cards Endpunkte
+import express from "express";
+import {
+  getCardsByDeck,
+  getCardById,
+  createCard,
+  updateCard,
+  deleteCard,
+} from "../controllers/cardController.js";
+ 
+const router = express.Router();
+ 
+router.route("/").post(createCard);
+router.route("/deck/:deckId").get(getCardsByDeck);
+router.route("/:id").get(getCardById).put(updateCard).delete(deleteCard);
+ 
+export default router;
--- backend/routes/deckRoutes.js
+++ backend/routes/deckRoutes.js
@@ -1,1 +1,16 @@
 // /api/decks Endpunkte
+import express from "express";
+import {
+  getDecks,
+  getDeckById,
+  createDeck,
+  updateDeck,
+  deleteDeck,
+} from "../controllers/deckController.js";
+ 
+const router = express.Router();
+ 
+router.route("/").get(getDecks).post(createDeck);
+router.route("/:id").get(getDeckById).put(updateDeck).delete(deleteDeck);
+ 
+export default router;
