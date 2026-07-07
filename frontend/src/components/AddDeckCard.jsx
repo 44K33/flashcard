b29967567961d949b8f55708a34c5b0cb7e3e59d
Lisbeth Häfliger <lisbeth.haefliger@edu.teko.ch>
2026-07-07 21:15:28 +0200

    AddDeckCard verfeinert

--- frontend/src/components/AddDeckCard.jsx
+++ frontend/src/components/AddDeckCard.jsx
@@ -2,8 +2,15 @@ import { PlusCircle } from "lucide-react";
 
 function AddDeckCard() {
   return (
-    <div className="border-dashed border-2 border-outline-variant items-center justify-center h-full rounded-xl flex flex-col hover:border-primary hover:text-primary transition-all duration-200">
-      <PlusCircle className="mb-4" />
+    // group = ermöglicht group-hover auf Kind-Elementen
+    // hover:border-primary hover:text-primary = wird beim Hovern blau
+    <div className="group border-dashed border-2 border-outline-variant items-center justify-center h-full rounded-xl flex flex-col hover:border-primary hover:text-primary transition-all duration-200 cursor-pointer">
+      {/* group-hover:scale-110 = Plus vergrössert sich wenn der ganze Container gehovert wird */}
+      {/* transition-transform = Vergrösserung animiert sich sanft */}
+      <PlusCircle
+        className="mb-4 group-hover:scale-110 transition-transform"
+        size={36}
+      />
       <p>Neuen Stapel erstellen</p>
     </div>
   );
