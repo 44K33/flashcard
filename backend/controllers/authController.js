5ec7c39bbb7ec36c20a3bf05884766f6c8c8f29c
AUV\flavio.koch <flavio.koch@auviso.ch>
2026-08-25 13:43:20 +0200

    authController erstellt und .env datei angepasst für das spätere Login

---
+++ backend/controllers/authController.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+// @desc    User einloggen
+// @route   POST /api/auth/login
+export const loginUser = async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    if (process.env.APP_PASSWORD && password === process.env.APP_PASSWORD) {
+      // Hier wird über prüft ob der wert "APP_PASSWORD" True/false ist
+      // dan wird es überprüft ob "password" mit "process.env.APP_PASSWORD" übereinstimmt
+
+      const token = jwt.sign({}, process.env.JWT_SECRET, { expiresIn: "1d" });
+      // jwt.sign(payload, secret, options) erstellt ein signiertes Token.
+      // Baut Header + Payload, kodiert beides (Base64Url) und berechnet mit JWT_SECRET
+      // eine Signatur (HMAC-SHA256) daraus. Ergebnis: "header.payload.signatur" als String.
+      // Nur wer JWT_SECRET kennt, kann gültige Tokens erzeugen oder später prüfen (jwt.verify).
+      // expiresIn setzt automatisch ein "exp"-Feld im Payload, damit das Token abläuft.
+
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({
+        message: "Ungültiges Passwort",
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Fehler beim Login", error: error.message });
+  }
+};
