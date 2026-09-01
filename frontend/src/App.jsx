a5d17efd6badd5eb86df6a506607ada088e040fa
Lisbeth Häfliger <lisbeth.haefliger@edu.teko.ch>
2026-09-01 11:16:46 +0200

    Login-Funktion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Study from "./pages/Study";
 import Navbar from "./components/Navbar";
 import CreateDeck from "./pages/CreateDeck";
+import Login from "./pages/Login";
 
 function App() {
   // useLocation = gibt die aktuelle URL zurück
@@ -13,16 +14,19 @@ function App() {
   const isStudy = location.pathname.startsWith("/study");
   // isCreateDeck = true wenn wir auf /decks/new sind, sonst false
   const isCreateDeck = location.pathname === "/decks/new";
+  // isLogin = true wenn wir auf /login sind, sonst false
+  const isLogin = location.pathname === "/login";
 
   return (
     <div className="min-h-screen bg-surface">
       {/* Navbar nur anzeigen wenn wir auf keiner der beiden fokussierten Seiten sind */}
-      {!isStudy && !isCreateDeck && <Navbar />}
+      {!isStudy && !isCreateDeck && !isLogin && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/decks/new" element={<CreateDeck />} />
         <Route path="/decks/:deckId" element={<DeckDetail />} />
         <Route path="/study/:deckId" element={<Study />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
